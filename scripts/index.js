289377aa2f5f96ee8c1da1f1d91f9cc223a700f1
Ayimaah <ayimaahtheodora2022@gmail.com>
2025-05-20 02:54:17 -0700

    portfolio

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // Declare variable to store navbar element
 const navElement =  document.getElementById('navbar');
-g// detect scroll event on document
+// detect scroll event on document
 document.addEventListener('scroll', function() {
     // console.log(window.scrollY);
     if (window.scrollY > 70) {
